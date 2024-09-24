6fa1ed49a88ab25d7f138e190c3f4bbba70a5c9d
Josh Ellis <30234012+senatormirror@users.noreply.github.com>
2024-09-24 18:36:25 +0100

    Discord.Net Initialisation

--- Program.cs
+++ Program.cs
@@ -1,5 +1,23 @@
-﻿public class Program {
-	public static void Main() {
-		Console.WriteLine("Printed from console");
+﻿using Discord;
+using Discord.WebSocket;
+
+public class Program {
+	private static DiscordSocketClient? _client;
+
+	public static async Task Main() {
+		_client = new DiscordSocketClient();
+		_client.Log += Log;
+
+		string? token = Environment.GetEnvironmentVariable("token");
+
+		await _client.LoginAsync(TokenType.Bot, token);
+		await _client.StartAsync();
+
+		await Task.Delay(-1);
+	}
+
+	private static Task Log(LogMessage msg) {
+		Console.WriteLine(msg.ToString());
+		return Task.CompletedTask;
 	}
 }
