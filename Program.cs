f866d3bd57e957d4c1ef92edf80994bec83e52e6
Josh Ellis <30234012+senatormirror@users.noreply.github.com>
2024-09-24 18:10:40 +0100

    Azure Deployment

--- Program.cs
+++ Program.cs
@@ -1,2 +1,5 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class Program {
+	public static void Main() {
+		Console.WriteLine("Printed from console");
+	}
+}
